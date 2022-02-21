f9613e6f7fc2dd16b144ed3e514d17227c238499
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-02-21 09:20:44 -0300

    Add files via upload

---
+++ assets/preload/images/Saku/wrath.lua
@@ -0,0 +1,28 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('sky', 'Saku/wrath_gates', -1500,-1000);
+	scaleObject('sky', 1.0, 1.0);
+	
+	makeLuaSprite('back', 'Saku/wrath_gates', -1500,-1000);
+	scaleObject('back', 1.0, 1.0);
+	
+   makeAnimatedLuaSprite('publico', 'Day/minus_back_characters', -1500, -590);
+	setLuaSpriteScrollFactor('publico', 1.0 , 1.0);
+	scaleObject('publico', 1.0, 1.0);
+	
+	  makeAnimatedLuaSprite('ventanas', 'Day/mics', -356, -540);
+	setLuaSpriteScrollFactor('ventanas', 1.0 , 1.0);
+	scaleObject('ventanas', 1.0, 1.0);
+
+	makeLuaSprite('over', 'Day/overlay1', -1600,-1100);
+	scaleObject('over', 1.05, 1.05);
+	
+	
+	addLuaSprite('back', false);
+	addLuaSprite('publico', false);
+	addAnimationByPrefix('publico', 'Idle', 'head bops', 24, true);
+	addLuaSprite('ventanas', true);
+	addAnimationByPrefix('ventanas', 'Idle', 'Day0', 24, true);
+	addLuaSprite('over', true);
+	
+	end
